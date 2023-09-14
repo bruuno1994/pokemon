ed58b9d78342cfcec06b38008e5aeb7709425ee8
bruuno1994 <bruuno1015@gmail.com>
2023-09-14 19:36:50 -0300

    projeto iniciado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './style.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const [pokemons, setPokemon] = useState([])
+  const [pokemonName, setPokemonName] = useState('')
+
+  useEffect(() => {
+
+    let url = `https://pokeapi.co/api/v2/pokemon/?limit=5&offset=0`;
+     fetch(url)
+     .then(response => response.json())
+     .then(res => {
+         setPokemon(res.results)
+       })
+     .catch(err => console.log(err))
+
+  },[])
+
+  console.log(pokemons);
+
+  const handleSearch = (e) => {
+    e.preventDefault(); // Impedir o envio do formulário
+  }
+
+  return(
+    <div className='container'>
+      <header>
+        <strong>Pokemon API</strong>
       </header>
+      <br />
+
+      <div>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Digite o nome do Pokémon"
+            value={pokemonName}
+            onChange={(e) => setPokemonName(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Buscar</button>
+        </form>
+        { pokemons.map((data, index) => (
+          <div key={index}>
+            <p >{ data.name }</p>
+          </div>
+        )) }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
